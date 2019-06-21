6f1b8a97769d2c0472452d4d3f27eecd98653bb7
NILESH PATIDAR <35222555+nileshpatidar@users.noreply.github.com>
2019-06-21 12:58:36 +0530

    local

--- s3_service/aws_s3.js
+++ s3_service/aws_s3.js
@@ -1,6 +1,8 @@
 var aws = require('aws-sdk')
 var multer = require('multer')
 var multerS3 = require('multer-s3')
+var mkdirp = require('mkdirp');
+mkdirp('uploads/test/');
 var s3 = new aws.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
@@ -9,6 +11,17 @@ var s3 = new aws.S3({
 //     accessKeyId: 'YOUR-ACCESS_KEY',
 //     secretAccessKey: 'YOUR_SECRET-ACCESS_KEY'
 // });
+
+var localstorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/test/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+})
+
+
 const fileFilter = (req, file, cb) => {
     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
         cb(null, true)
@@ -16,6 +29,7 @@ const fileFilter = (req, file, cb) => {
         return cb('jpeg,jpg ,png file are allowed',false)
     }
 }
+
 const storage = multerS3({
     s3: s3,
     bucket: 'yourbucketname',
@@ -27,11 +41,19 @@ const storage = multerS3({
         cb(null, Date.now().toString() + '.' + file.originalname.split('.')[file.originalname.split('.').length - 1]) //image name with timestamp
     }
 })
-
-
 var upload = multer({
     storage: storage,
     fileFilter:fileFilter,
-})
+}).single('image')
 
-module.exports = upload
+
+var local_upload = multer({
+    storage:localstorage,
+    fileFilter: fileFilter,
+}).array('photos', 5)
+
+
+
+
+module.exports.upload = upload ; 
+module.exports.local_upload=local_upload;
